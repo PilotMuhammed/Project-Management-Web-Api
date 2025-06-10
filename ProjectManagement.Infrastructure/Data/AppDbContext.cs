667d57de5a502056414733ea4a13286ea83ef9e1
pilotmuhammed <mohammeditsec@gmail.com>
2025-06-10 17:41:31 +0300

    Add AppDbContext inside Infrastructure Layer

---
+++ ProjectManagement.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagement.Domain.Models;
+
+namespace ProjectManagement.Infrastructure.Data
+{
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<Priority> Priorities { get; set; }
+        public DbSet<Milestone> Milestones { get; set; }
+        public DbSet<Attachment> Attachments { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<ActivityLog> ActivityLogs { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<TaskTag> TaskTags { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // تعريف المفتاح المركب لجدول الربط
+            modelBuilder.Entity<TaskTag>()
+                .HasKey(tt => new { tt.TaskId, tt.TagId });
+        }
+    }
+
+}
