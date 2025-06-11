e0c24f2cdcfc975136a8efe8d176ea8dfc92927f
pilotmuhammed <mohammeditsec@gmail.com>
2025-06-11 15:06:06 +0300

    Edit AppDb

--- ProjectManagement.Infrastructure/Data/AppDbContext.cs
+++ ProjectManagement.Infrastructure/Data/AppDbContext.cs
@@ -14,7 +14,7 @@ namespace ProjectManagement.Infrastructure.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Project> Projects { get; set; }
-        public DbSet<Task> Tasks { get; set; }
+        public DbSet<TaskWork> Tasks { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<Status> Statuses { get; set; }
